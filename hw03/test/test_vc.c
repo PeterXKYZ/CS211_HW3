99eae93c038d7eb76607a4c126b6a7e04f3425a6
Zhuoer Peter Zhou <ppz4725@gotham.ece.northwestern.edu>
2022-01-27 21:39:23 -0600

    final few tests

--- hw03/test/test_vc.c
+++ hw03/test/test_vc.c
@@ -80,6 +80,11 @@ static void test_min_max(void) {
         return;
     }
 
+    CHECK_POINTER( vc_max(vc), NULL); // vc_max of an empty map should be NULL
+    CHECK_POINTER( vc_min(vc), NULL); // vc_min of an empty map should be NULL
+    CHECK_SIZE( vc_total(vc), 0); // vc_total of an empty map should be 0
+    
+    
     size_t* cp = vc_update(vc, "alice");
     CHECK( cp );
 
@@ -97,11 +102,11 @@ static void test_min_max(void) {
 
     cp = vc_update(vc, "nut");
 
-    CHECK_SIZE( vc_lookup(vc, "alice"), 0);
-    CHECK_SIZE( vc_lookup(vc, "hank"), 10 );
-    CHECK_STRING( vc_max(vc), "bob" );
-    CHECK_STRING( vc_min(vc), "hank" );
-    CHECK_SIZE( vc_total(vc), 62);
+    CHECK_SIZE( vc_lookup(vc, "alice"), 0); // vc_update created "alice"
+    CHECK_SIZE( vc_lookup(vc, "hank"), 10 ); // vc_update updated "hank"
+    CHECK_STRING( vc_max(vc), "bob" ); // when tied, return max of the earliest enty
+    CHECK_STRING( vc_min(vc), "hank" ); // when tied, return min of the latest entry
+    CHECK_SIZE( vc_total(vc), 62); // total works
     vc_destroy(vc);
 }
 
@@ -129,6 +134,25 @@ static void test_lookup(void) {
     vc_destroy(vc);
 }
 
+
+static void variable_max_candidates(void){
+
+    vote_count_t vc = vc_create();
+    size_t* t;
+
+    char s[20];
+    for(int i=0; i <  MAX_CANDIDATES; i++){
+        snprintf(s, 20, "bush %d", i);
+        t = vc_update(vc, s);
+        *t +=1;
+    }
+    
+    CHECK(vc_update(vc, "obama")==NULL);
+    CHECK(vc_update(vc,"joe")==NULL);
+    vc_print(vc);
+    vc_destroy(vc);
+}
+
 int main(void)
 {
     test_create_destroy();
@@ -136,6 +160,7 @@ int main(void)
     test_creation();
     test_min_max();
     test_lookup();
+    variable_max_candidates();
     return 0;
 }
 
